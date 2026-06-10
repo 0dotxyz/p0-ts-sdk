6ce21795f499ba1e796d8e701493c7201f430c7b
Kobe <leenderskobe@gmail.com>
2026-06-10 11:23:28 +0200

    feat: add jupiter idl export

--- src/vendor/jup-lend/idl/index.ts
+++ src/vendor/jup-lend/idl/index.ts
@@ -1,5 +1,10 @@
 import { JupLend } from "./jup-lend";
 import JUP_LEND_IDL_JSON from "./jup-lend.json";
+import { Liquidity } from "./jup-lend-liquidity";
+import JUP_LIQUIDITY_IDL_JSON from "./jup-lend-liquidity.json";
 
 export const JUP_LEND_IDL = JUP_LEND_IDL_JSON as unknown as JupLend;
 export type JupLendIdlType = JupLend;
+
+export const JUP_LIQUIDITY_IDL = JUP_LIQUIDITY_IDL_JSON as unknown as Liquidity;
+export type JupLiquidityIdlType = Liquidity;
