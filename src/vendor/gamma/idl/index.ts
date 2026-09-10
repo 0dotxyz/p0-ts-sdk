cbe03d7016c5eee92dd121095ca62a13983b86f3
Kobe <leenderskobe@gmail.com>
2026-09-10 16:59:08 +0200

    feat: bumped gamma program version

--- src/vendor/gamma/idl/index.ts
+++ src/vendor/gamma/idl/index.ts
@@ -1,5 +1,5 @@
-import { GammaVault } from "./gamma-idl-2";
-import GAMMA_VAULT_IDL_JSON from "./gamma-idl-2.json";
+import { GammaVault } from "./gamma-idl";
+import GAMMA_VAULT_IDL_JSON from "./gamma-idl.json";
 
 export const GAMMA_VAULT_IDL = GAMMA_VAULT_IDL_JSON as unknown as GammaVault;
 export type GammaVaultIdlType = GammaVault;
